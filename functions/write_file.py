097e95dd1195e3f9a3bd958186e901e3ff55fc6b
Kam1217 <kamilaostroga@gmail.com>
2025-06-12 14:11:11 +0100

    Write file function, print error if we are working outside of working directory, check if file exists and create it if not

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,3 +1,15 @@
+import os
 
 def write_file(working_directory, file_path, content):
-    pass
+    
+    file_directory_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not os.path.abspath(file_directory_path).startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot write to "{file_path}" as it is outsidee the permitted working directory'
+      
+    try:
+        directory = os.path.dirname(file_directory_path)
+        os.makedirs(directory, exist_ok=True)
+  
+    except Exception as e:
+        return f"Error: {e}"
