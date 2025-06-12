943014da2ac441c3f0c4e307ef31c9701d6f9cc3
Kam1217 <kamilaostroga@gmail.com>
2025-06-12 10:59:33 +0100

    add --verbose to display extra details such as user prompt, prompt tokens, response tokens. Hide if info if not used and only display the anwser to the prompt

--- main.py
+++ main.py
@@ -8,15 +8,25 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+verbose = "--verbose" in sys.argv
+
 if len(sys.argv) == 1:
     print("No prompt provided")
     sys.exit(1)
 
-messages = [types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),]
+if verbose:
+    prompt = " ".join(sys.argv[1:-1])
+else:
+    prompt = " ".join(sys.argv[1:])
+
+messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
 response = client.models.generate_content(model='gemini-2.0-flash-001', contents= messages)
 
-
+if verbose:
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
