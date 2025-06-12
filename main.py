c8db9eadfab538822ed5a34c12faf06bcc1251bb
Kam1217 <kamilaostroga@gmail.com>
2025-06-12 18:19:18 +0100

    Add all of the previously functions to the ai_agent so it can run them

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
@@ -42,9 +45,59 @@ schema_get_files_info = types.FunctionDeclaration(
     ),
 )
 
+schema_get_file_content = types.FunctionDeclaration(
+    name = "get_file_content",
+    description="Returns the contents of a specified file up to 1000 characters",
+    parameters=types.Schema(
+        type = types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to read file contents from, relative to the working directory. If file is not provided, return an error specifying the file does not exist.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name = "run_python_file",
+    description="Executes Python files with optional arguments",
+    parameters= types.Schema(
+        type = types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to run the python script from, relative to the working directory. If file is not provided or is not a python file, return an error specifying the file does not exist.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name = "write_file",
+    description="Overwrites the contents of a specified file. If the file does not exist it creates it with the contents instead",
+    parameters= types.Schema(
+        type = types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file which will have the overwritten content, relative to the working directory. If file is not provided it creates the files with the specified contents",
+
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The contents to overwrite a file with. If a file does not exists create a file with the contents instead"
+            )
+        },
+    ),
+)
+
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
     ]
 )
 
