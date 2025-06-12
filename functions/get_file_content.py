a9afe28d5de5da92fc114f87e4ac1928c5e43ae5
Kam1217 <kamilaostroga@gmail.com>
2025-06-12 12:47:24 +0100

    Print error if file does not exist or is not in working directory in the get_file_content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,14 @@
 import os
 
 def get_file_content(working_directory, file_path):
-    pass
+
+    if file_path is None:
+        file_directory_path = os.path.abspath(working_directory)
+    else:
+        file_directory_path = os.path.abspath(os.path.join(working_directory, file_path))   
+
+    if not os.path.isfile(file_directory_path):
+        return f'Error: File not found or is not regular file: "{file_directory_path}"'
+    
+    if not os.path.abspath(file_directory_path).startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot read "{file_path}" as it is outsidee the permitted working directory'
