768ed62d86108e955287a71628f5a19ef04f44f8
Kam1217 <kamilaostroga@gmail.com>
2025-06-12 15:01:43 +0100

    in run_python_file function return str errors if file is not within working directory, file path is not found and file does not end with .py

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -4,8 +4,11 @@ def run_python_file(working_directory, file_path):
     
     file_directory_path = os.path.abspath(os.path.join(working_directory, file_path))   
 
-    if not os.path.isfile(file_directory_path):
-        return f'Error: File "{file_path} not found"'
-    
     if not os.path.abspath(file_directory_path).startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.exists(file_directory_path):
+        return f'Error: File "{file_path} not found"'
+    
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
