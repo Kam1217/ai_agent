787aeb24ed064399de6c13efa1d7b7c75f4f0ad8
Kam1217 <kamilaostroga@gmail.com>
2025-06-12 16:12:21 +0100

    set a system_prompt so the ai agent only responds with I AM A ROBOT

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
 verbose = "--verbose" in sys.argv
 
 if len(sys.argv) == 1:
@@ -20,7 +21,7 @@ else:
     prompt = " ".join(sys.argv[1:])
 
 messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
-response = client.models.generate_content(model='gemini-2.0-flash-001', contents= messages)
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents= messages, config= types.GenerateContentConfig(system_instruction=system_prompt))
 
 if verbose:
     print(f"User prompt: {prompt}")
