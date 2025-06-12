23fb37393f702d56f385549fe4be24a93d2a7e95
Kam1217 <kamilaostroga@gmail.com>
2025-06-12 11:48:13 +0100

    Build and return a strig representing the contents of the directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,3 +7,15 @@ def get_files_info(working_directory, directory=None):
     
     if not os.path.isdir(directory):
         return f'Error: "{directory}" is not a directory'
+    
+    results = []
+    for item in os.listdir(directory):
+        path_to_item = os.path.join(directory, item)
+        
+        file_size = os.path.getsize(path_to_item)
+        is_dir = os.path.isdir(path_to_item)
+
+        line = f"- {item}: file_size={file_size} bytes, is_dir={is_dir}"
+        results.append(line)
+
+    return "\n".join(results)
