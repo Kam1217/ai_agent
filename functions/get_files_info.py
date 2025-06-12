c5a2508be0cf3ba6332220c178fa2b7c6239e54e
Kam1217 <kamilaostroga@gmail.com>
2025-06-12 12:35:27 +0100

    Fix get_files_info so it accessed the working directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,17 +1,22 @@
 import os
 
 def get_files_info(working_directory, directory=None):
+
+    if directory is None:
+        directory_path = os.path.abspath(working_directory)
+    else:
+        directory_path = os.path.abspath(os.path.join(working_directory, directory))
     
-    if not os.path.abspath(directory).startswith(os.path.abspath(working_directory)):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    
-    if not os.path.isdir(directory):
+    if not os.path.isdir(directory_path):
         return f'Error: "{directory}" is not a directory'
     
+    if not os.path.abspath(directory_path).startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
     try:
         results = []
-        for item in os.listdir(directory):
-            path_to_item = os.path.join(directory, item)
+        for item in os.listdir(directory_path):
+            path_to_item = os.path.join(directory_path, item)
         
             file_size = os.path.getsize(path_to_item)
             is_dir = os.path.isdir(path_to_item)
@@ -22,4 +27,3 @@ def get_files_info(working_directory, directory=None):
         return "\n".join(results)
     except Exception as e:
         return f"Error: {e}"
-
