f6c7282b5461accaba5879912ab78cb729d874b8
Kam1217 <kamilaostroga@gmail.com>
2025-06-12 12:03:29 +0100

    Test results and errors work for the get_file_info functions

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
-import unittest
 from functions.get_files_info import get_files_info
 
-class TestGetFileInfo(unittest.TestCase):
+if __name__ == "__main__":
+    print(f"Should print result: {get_files_info('calculator', '.')}")
+    print(f"Should print result: {get_files_info('calculator', 'pkg')}")
+    print(f"Should print error: {get_files_info('calculator', '/bin')}")
+    print(f"Should print error: {get_files_info('calculator', '../')}")
