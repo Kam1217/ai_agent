d7397bce4b5f734a9cc4b9b918a3f40c428dda73
Kam1217 <kamilaostroga@gmail.com>
2025-06-13 09:27:49 +0100

    set up a call function and print function_call.name and function_call.args if verebose is sepcified

---
+++ functions/call_function.py
@@ -0,0 +1,6 @@
+def call_function(function_call, verbose=False):
+    
+    if verbose:
+        print(f"- Calling function: {function_call.name}({function_call.args})")
+    print("f - Calling function: {function_call.name}")
+
